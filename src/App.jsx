07226008715533a719946d68c0f240bde0f3878d
wen7752 <wen7752@users.noreply.github.com>
2022-07-27 23:11:09 -0700

    Worked on components and general css styling

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Footer from './components/footer/Footer'
+import Testimonials from './components/testimonials/Testimonials'
+import Portfolio from './components/portfolio/Portfolio'
+import Contact from './components/contact/Contact'
+import Services from './components/services/Services'
+
+
+const App = () => {
+  return (
+    <>
+        <Header/>
+        <Nav />
+        <About />
+        <Experience/>
+        <Services/>
+        <Portfolio/>
+        <Testimonials/>
+        <Contact/>
+        <Footer/>
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+
+
+
+
+const Header = () => {
+  return (
+    <header>
+      
+      <div className='container header__container'>
+
+        <h5>Hello I'm </h5>
+        <h1>Wen Fang</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+
+      </div>
+
+
+    </header>
+  )
+}
+
+export default Header
